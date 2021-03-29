e89518bc47bf59eaa5938df884ca0e3f3a98b1b9
愛甲知央 <78640440+aikochihiro@users.noreply.github.com>
2021-03-29 23:21:48 +0900

    2章チェックテスト

--- JavaStudy/Check2/src/Check/Check.java
+++ JavaStudy/Check2/src/Check/Check.java
@@ -1,13 +1,26 @@
-package Check;
+package check;
 
+
+import constants.Constants;
 
 public class Check {
+	private String firstName = "愛甲";
+	private String lastName = "知央";
+
+	private void printName() {
+		System.out.println("printNameメソッド → " + firstName + lastName);
+
+	}
 
 	public static void main(String[] args) {
+		Check c = new Check();
+		c.printName();
 
-		private String firstName ="愛甲";
-		private String lastName ="知央";
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA, Constants.CHECK_CLASS_HOGE);
+         pet.introduce();
 
+         RobotPet rpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+	     rpet.introduce();
 	}
 
 }
